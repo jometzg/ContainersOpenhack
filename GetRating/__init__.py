53c560753c8ed22d8c8373764d7ea9a62ca609cb
John Metzger <john.metzger@microsoft.com>
2021-04-12 12:00:05 +0000

    getRating from Cosmos

--- GetRating/__init__.py
+++ GetRating/__init__.py
@@ -1,18 +1,24 @@
 import logging
 
 import azure.functions as func
+import json
 
-
-def main(req: func.HttpRequest) -> func.HttpResponse:
+def main(req: func.HttpRequest, rating: func.DocumentList) -> str:
     logging.info('Python HTTP trigger function processed a request.')
-
-    ratingid = req.params.get('ratingId')
- 
-
-    if ratingid:
-        return func.HttpResponse(f"Hello, {ratingid}. This HTTP triggered function executed successfully.")
+    logging.info(req.params["userId"])
+    if not rating:
+        logging.warning("rating item not found")
+        return func.HttpResponse(f"Not foound", status_code=404)
     else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
+        logging.info("Found rating item, Notes=%s",
+                     rating[0]['UserNotes'])
+        data = {
+            "id": rating[0]["id"],
+            "userId" : rating[0]["userId"],
+            "ProductId" : rating[0]["ProductId"],
+            "timestamp": rating[0]["timestamp"],
+            "locationName": rating[0]["locationName"],  
+            "Rating" : rating[0]["Rating"],
+            "UserNotes" : rating[0]["UserNotes"],
+        }
+        return json.dumps(data)
